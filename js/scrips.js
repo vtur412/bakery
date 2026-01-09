c4e8c38415fdd167fd5c697d818fde20ebaaafb7
v.t412 <ttt@ttt.tt>
2026-01-09 23:16:40 +0200

    code refactoring all section (wthout footer)

--- js/scrips.js
+++ js/scrips.js
@@ -2,7 +2,7 @@ const modal = document.querySelector(".backdrop");
 const modalBtnOpen = document.querySelector(".modal-btn-open");
 const modalBtnClose = document.querySelector(".modal-btn-close");
 
-const toggleModal = () => modal.classList.toggle("is-hiden");
+const toggleModal = () => modal.classList.toggle("is-hidden");
 
 modalBtnOpen.addEventListener("click", toggleModal);
 modalBtnClose.addEventListener("click", toggleModal);
